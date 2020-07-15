4dc27dc422490aeb7260f5cdf5e204a047844b26
Barbora Darmová <barbora.darmova@virtuplex.cz>
2020-07-15 17:08:16 +0200

    Scale start

--- Assets/GizmoPack/Scripts/ScaleAxis.cs
+++ Assets/GizmoPack/Scripts/ScaleAxis.cs
@@ -7,12 +7,45 @@ public class ScaleAxis : Axis
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitAxis();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnMouseDown()
+    {
+        StartMouseDrag();
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 delta, pos;
+        mousePos = UpdateMouseDrag(mousePos, out delta, out pos);
+
+        switch (axis)
+        {
+            case GizmoAxis.xAxis:
+                pos.x += delta.x;
+                break;
+            case GizmoAxis.yAxis:
+                pos.y += delta.y;
+                break;
+            case GizmoAxis.zAxis:
+                pos.z += delta.z;
+                break;
+        }
+
+        root.transform.position = pos;
+
+        lastMousePos = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    private void OnMouseUp()
+    {
+        gizmoController.ReleaseController();
     }
 }
